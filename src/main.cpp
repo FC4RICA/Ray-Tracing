c19ec315d700a0140bae1606025516d6d9622974
FC4RICA <kantinan.fw@gmail.com>
2024-07-03 13:35:29 +0700

    Add fuzz to the metal material

--- src/main.cpp
+++ src/main.cpp
@@ -12,8 +12,8 @@ int main() {
 	hittable_list world;
 	auto material_ground = make_unique<lambertian>(color(0.8, 0.8, 0.0));
 	auto material_center = make_unique<lambertian>(color(0.1, 0.2, 0.5));
-	auto material_left = make_unique<metal>(color(0.8, 0.8, 0.8));
-	auto material_right = make_unique<metal>(color(0.8, 0.6, 0.2));
+	auto material_left = make_unique<metal>(color(0.8, 0.8, 0.8), 0.3);
+	auto material_right = make_unique<metal>(color(0.8, 0.6, 0.2), 1.0);
 
 	world.add(make_unique<sphere>(point3(0.0, -100.5, -1.0), 100.0, std::move(material_ground)));
 	world.add(make_unique<sphere>(point3(0.0, 0.0, -1.2), 0.5, std::move(material_center)));
--- src/material.h
+++ src/material.h
@@ -32,17 +32,19 @@ private:
 
 class metal : public material {
 public:
-	metal(const color& albedo) : albedo(albedo) {}
+	metal(const color& albedo, double fuzz) : albedo(albedo), fuzz(fuzz < 1 ? fuzz : 1) {}
 
 	bool scatter(const ray& ray_in, const hit_record& rec, color& attenuation, ray& scattered) const override {
 		auto reflexed_dir = reflex(ray_in.direction(), rec.normal);
+		reflexed_dir = unit_vector(reflexed_dir) + (fuzz * random_unit_vec());
 		scattered = ray(rec.p, reflexed_dir);
 		attenuation = albedo;
-		return true;
+		return (dot(scattered.direction(), rec.normal) > 0);
 	}
 
 private:
 	color albedo;
+	double fuzz;
 };
 
 #endif // !MATERIAL_H
